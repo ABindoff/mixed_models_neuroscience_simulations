0b56290107b29ef314418010b3c056cc6758f18f
Aidan Bindoff <Aidan.Bindoff@utas.edu.au>
2018-05-15 16:18:05 +1000

    functions

--- nested.R
+++ nested.R
@@ -5,33 +5,59 @@ library(lme4)
 library(gridExtra)
 library(lmerTest)
 library(viridis)
-ggplot <- function(...) ggplot2::ggplot(...) + scale_colour_viridis(discrete = T) + scale_fill_viridis(discrete = T)
-
+library(ggbeeswarm)
+ggplot <- function(...) ggplot2::ggplot(...) + scale_colour_viridis(discrete = T, end = 0.5) + scale_fill_viridis(discrete = T, end = 0.5)
+options(digits = 3)
 set.seed(1)
 
+sim.mm <- function(replicates = 6, treatment = 0.5, sd1 = 0.5, sd2 = 1){
+  replicates <- 2*(floor(replicates/2))
+  replicate1 <- c(1:floor(replicates/2))
+  replicate2 <- c((floor(replicates/2)+1):replicates) 
+  d1 <- expand.grid(animalid  = c(-0.5, 0.1, 0.4),
+                    treatment = c(0, treatment),
+                    replicate = replicate1)
+  d2 <- expand.grid(animalid  = c(-0.5, 0.1, 0.4),
+                    treatment = c(0, treatment),
+                    replicate = replicate2)
+  # smaller effect sizes (random and fixed)
+  small.noise <- abs(rnorm(nrow(d1), 0, sd1))
+  d1$y1 <- d1$treatment + d1$animalid + small.noise
+  d2$y1 <- d2$treatment + d2$animalid - small.noise
+  # larger effect sizes (random and fixed)
+  large.noise <- abs(rnorm(nrow(d1), 0, sd2))
+  d1$y2 <- d1$treatment + d1$animalid + large.noise
+  d2$y2 <- d2$treatment + d2$animalid - large.noise
+  
+  d <- bind_rows(d1, d2)
+  
+  
+  d$animalid %<>% factor(labels = c("01", "02", "03"))
+  d$treatment %<>% factor(labels = c("a", "b"))
+  d %<>% group_by(treatment, animalid) %>% mutate(y1mean = mean(y1), y2mean = mean(y2))
+  d
+}
 
-d <- expand.grid(animalid  = c(-0.5, 0, 0.5),
-                 treatment = c(0, 0.5),
-                 replicate = c(1:10))
-# large effect sizes (random and fixed)
-d$y1 <- d$treatment + d$animalid + rnorm(nrow(d), 0, 0.2)
-# small effect sizes (random and fixed)
-d$y2 <- d$treatment + d$animalid + rnorm(nrow(d), 0, 2)
-d$animalid %<>% factor(labels = c("01", "02", "03"))
-d$treatment %<>% factor(labels = c("a", "b"))
-d %<>% group_by(treatment, animalid) %>% mutate(y1mean = mean(y1), y2mean = mean(y2))
 
-p1 <- ggplot(d, aes(x = treatment, y = y1, colour = animalid, fill = animalid)) +
-  geom_point(position = position_dodge(width = 1/2), alpha = 3/4) +
-  geom_point(aes(y = y1mean), size = 4, position = position_dodge(width = 1/2), shape = 3) +
-  ylim(-3, 3)
-p2 <- ggplot(d, aes(x = treatment, y = y2, colour = animalid)) +
-  geom_point(position = position_dodge(width = 1/2), alpha = 3/4) +
-  geom_point(aes(y = y2mean), size = 4, position = position_dodge(width = 1/2), shape = 3) +
-  ylim(-3, 3)
 
-grid.arrange(p1, p2, ncol = 2)
 
+plot.sim <- function(d){
+  p1 <- ggplot(d, aes(x = treatment, y = y1, colour = animalid, fill = animalid)) +
+    geom_quasirandom(dodge.width = 1, alpha = 1/3) +
+    geom_point(aes(y = y1mean), size = 4, position = position_dodge(width = 1), shape = 3) +
+    ylim(-3, 3)
+  p2 <- ggplot(d, aes(x = treatment, y = y2, colour = animalid)) +
+    geom_quasirandom(dodge.width = 1, alpha = 1/3) +
+    geom_point(aes(y = y2mean), size = 4, position = position_dodge(width = 1), shape = 3) +
+    ylim(-3, 3)
+
+  grid.arrange(p1, p2, ncol = 2)
+}
+
+d <- sim.mm()
+plot.sim(d)
+
+# which one would you trust more?
 
 
 # recover fixed and random parameters using `lmer`
@@ -45,8 +71,8 @@ m2 <- lmer(y2 ~ treatment + (treatment|animalid), data = d)
 ranef(m2)
 fixef(m2)  # obviously not as good as y1, but not terrible
 
-anova(m1)  # justifiably reject null
-anova(m2)  
+anova(m1, ddf = "Kenward-Roger")  # justifiably reject null
+anova(m2, ddf = "Kenward-Roger")  
 
 # what happens if we don't account for random effect?
 # y1
@@ -64,28 +90,9 @@ anova(m4)
 # what happens if we increase the number of replicates?
 set.seed(1)
 
+d <- sim.mm(40)
 
-d <- expand.grid(animalid  = c(-0.5, 0, 0.5),
-                 treatment = c(0, 0.5),
-                 replicate = c(1:500))
-# large effect sizes (random and fixed)
-d$y1 <- d$treatment + d$animalid + rnorm(nrow(d), 0, 0.2)
-# small effect sizes (random and fixed)
-d$y2 <- d$treatment + d$animalid + rnorm(nrow(d), 0, 2)
-d$animalid %<>% factor(labels = c("01", "02", "03"))
-d$treatment %<>% factor(labels = c("a", "b"))
-d %<>% group_by(treatment, animalid) %>% mutate(y1mean = mean(y1), y2mean = mean(y2))
-
-p1 <- ggplot(d, aes(x = treatment, y = y1, colour = animalid, fill = animalid)) +
-  geom_point(position = position_dodge(width = 1/2), alpha = 3/4) +
-  geom_point(aes(y = y1mean), size = 4, position = position_dodge(width = 1/2), shape = 3) +
-  ylim(-3, 3)
-p2 <- ggplot(d, aes(x = treatment, y = y2, colour = animalid)) +
-  geom_point(position = position_dodge(width = 1/2), alpha = 3/4) +
-  geom_point(aes(y = y2mean), size = 4, position = position_dodge(width = 1/2), shape = 3) +
-  ylim(-3, 3)
-
-grid.arrange(p1, p2, ncol = 2)
+plot.sim(d)
 
 
 
@@ -98,7 +105,43 @@ fixef(m1)  # so far, so good
 # y2
 m2 <- lmer(y2 ~ treatment + (treatment|animalid), data = d)
 ranef(m2)
-fixef(m2)  # obviously not as good as y1, but not terrible
+fixef(m2)  
 
 anova(m1, ddf = "Kenward-Roger")  # justifiably reject null
 anova(m2, ddf = "Kenward-Roger")  
+
+
+m3 <- lm(y1 ~ treatment, data = d)
+anova(m3)
+m4 <- lm(y2 ~ treatment, data = d)
+anova(m4)
+
+
+# we can see that m3 and m4 overestimate degrees of freedom
+# not such a problem when we KNOW that there is an effect, right?
+# but what if we KNOW that there ISN'T?
+
+d <- sim.mm(replicates = 10, treatment = 1e-16)
+plot.sim(d)
+
+# recover fixed and random parameters using `lmer`
+# y1
+m1 <- lmer(y1 ~ treatment + (treatment|animalid), data = d)
+ranef(m1)
+fixef(m1)  # so far, so good
+
+# y2
+m2 <- lmer(y2 ~ treatment + (treatment|animalid), data = d)
+ranef(m2)
+fixef(m2)  
+
+anova(m1, ddf = "Kenward-Roger")  # justifiably reject null
+anova(m2, ddf = "Kenward-Roger")  
+
+m3 <- lm(y1 ~ treatment, data = d)
+anova(m3)
+m4 <- lm(y2 ~ treatment, data = d)
+anova(m4)
+
+
+
